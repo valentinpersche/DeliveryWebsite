8cb87791434834c01319f353405e7fc5a06a8da4
valentinpersche <valentin.persche@icloud.com>
2025-04-28 18:59:19 +0200

    update folder structure and add db.js for dishes array

---
+++ scripts/db.js
@@ -0,0 +1,104 @@
+let myDishes = [
+  {
+    category: "Pasta",
+    image: "",
+    items: [
+      {
+        name: "Spaghetti Bolognese",
+        price: 14.99,
+        description:
+          "Spaghetti mit Hackfleisch, Karotten, Sellerie und Zwiebeln in Tomatensoße",
+      },
+      {
+        name: "Spaghetti Carbonara",
+        price: 12.99,
+        description: "Spaghetti mit Speck, Eiern und Parmesan",
+      },
+      {
+        name: "Spaghetti Alfredo",
+        price: 12.99,
+        description: "Spaghetti mit Parmesan, Sahne und Butter",
+      },
+      {
+        name: "Spaghetti Puttanesca",
+        price: 14.99,
+        description: "Spaghetti mit Tomaten, Oliven, Knusperschinken und Feta",
+      },
+    ],
+  },
+  {
+    category: "Pizza",
+    items: [
+      {
+        name: "Pizza Margherita",
+        price: 12.99,
+        description: "Pizza mit Tomaten, Mozzarella und Basilikum",
+      },
+      {
+        name: "Pizza Salami",
+        price: 12.99,
+        description: "Pizza mit Salami, Mozzarella und Basilikum",
+      },
+      {
+        name: "Pizza Funghi",
+        price: 12.99,
+        description: "Pizza mit Pilzen, Mozzarella und Basilikum",
+      },
+      {
+        name: "Pizza Vegetarian",
+        price: 12.99,
+        description: "Pizza mit Vegetables, Mozzarella und Basilikum",
+      },
+    ],
+  },
+  {
+    category: "Salate",
+    items: [
+      {
+        name: "Caesar Salad",
+        price: 12.99,
+        description: "Römerssalat, Croutons, Parmesan, Hähnchenbrust, Dressing",
+      },
+      {
+        name: "Greek Salad",
+        price: 12.99,
+        description: "Gurke, Tomaten, Zwiebeln, Feta, Oliven, Dressing",
+      },
+      {
+        name: "Tuna Salad",
+        price: 12.99,
+        description: "Thunfisch, Ei, Gurke, Zwiebeln, Feta, Oliven, Dressing",
+      },
+    ],
+  },
+  {
+    category: "Getränke",
+    items: [
+      {
+        name: "Wasser",
+        price: 1.99,
+        description: "Mineralwasser 0.33l",
+      },
+      {
+        name: "CocaCola",
+        price: 2.99,
+        description: "Cola 0.33l",
+      },
+      {
+        name: "Fanta",
+        price: 2.99,
+        description: "Fanta 0.33l",
+      },
+      {
+        name: "Sprite",
+        price: 2.99,
+        description: "Sprite 0.33l",
+      },
+      {
+        name: "Apfelschorle",
+        price: 2.99,
+        description: "Apfelschorle 0.33l",
+      },
+    ],
+  },
+];
